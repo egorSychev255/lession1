aa8d883feba2ee431ecd24e6d85b4f4a7932defa
Egor <egorca255@mail.ru>
2022-09-13 22:34:27 +0300

    Сложение рандомных чисел

--- Ex002_СложениеЧисел/Program.cs
+++ Ex002_СложениеЧисел/Program.cs
@@ -1,3 +1,7 @@
-﻿int numberA = 3;
-int numberB = 4;
+﻿int numberA = new Random().Next(1,10);
+Console.Write("Первое число=");
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1,10);
+Console.Write("Второе число=");
+Console.WriteLine(numberB);
 Console.WriteLine(numberA + numberB);
